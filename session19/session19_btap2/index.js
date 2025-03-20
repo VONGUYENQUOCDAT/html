a2c284037ba9a8cbfdd0de751823a8f12936025d
VONGUYENQUOCDAT <vodatt165@gmail.com>
2025-03-20 09:02:47 +0700

    Delete session19 directory

--- session19/session19_btap2/index.js
+++
@@ -1,7 +0,0 @@
-let input = prompt("Nhập một ký tự: ");
-
-if (input.length === 1 && /^[a-zA-Z]$/.test(input)) {
-    alert(`Ký tự ${input} là chữ cái`);
-} else {
-    alert("Không phải chữ cái");
-}
--- session19/session19_btap5/index.js
+++
@@ -1,6 +0,0 @@
-let input = prompt("Nhập một chuỗi: ");
-if (input.includes(" ") == true) {
-    alert("Chuỗi chứa dấu cách");
-} else {
-    alert("Chuỗi không chứa dấu cách");
-}
